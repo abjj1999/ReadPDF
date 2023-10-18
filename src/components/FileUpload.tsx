ef1eb86aaa13eb3a6dc76474e785ffe2bd51a62e
Abdullah al hilfi <abdullahalhilfi21@gmail.com>
2023-10-18 13:19:25 -0500

    idk what's wrong

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,95 +1,96 @@
-"use client"
-import { uploadToS3 } from '@/lib/s3';
-import { useMutation } from '@tanstack/react-query';
-import axios from 'axios';
-import { Inbox, Loader2 } from 'lucide-react';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-import {useDropzone} from 'react-dropzone'
-import toast from 'react-hot-toast';
-interface FileUploadProps {}
+"use client";
+import { uploadToS3 } from "@/lib/s3";
+import { useMutation } from "@tanstack/react-query";
+import { Inbox, Loader2 } from "lucide-react";
+import React from "react";
+import { useDropzone } from "react-dropzone";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
-const  FileUpload = () => {
-    const [uploading, setUploading] = useState(false);
-    const router = useRouter();
-    const  {mutate, isLoading} = useMutation({
-        mutationFn: async({file_key, file_name}:{file_key: string, file_name: string}) => {
-            const res = await axios.post("/api/chat/create",{
-                file_key, file_name
-            })
-            // console.log(res.data);
-            return res.data;
+// https://github.com/aws/aws-sdk-js-v3/issues/4126
+
+const FileUpload = () => {
+  const router = useRouter();
+  const [uploading, setUploading] = React.useState(false);
+  const { mutate, isLoading } = useMutation({
+    mutationFn: async ({
+      file_key,
+      file_name,
+    }: {
+      file_key: string;
+      file_name: string;
+    }) => {
+      const response = await axios.post("/api/chat/create", {
+        file_key,
+        file_name,
+      });
+      return response.data;
+    },
+  });
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: { "application/pdf": [".pdf"] },
+    maxFiles: 1,
+    onDrop: async (acceptedFiles) => {
+      const file = acceptedFiles[0];
+      if (file.size > 10 * 1024 * 1024) {
+        // bigger than 10mb!
+        toast.error("File too large");
+        return;
+      }
+
+      try {
+        setUploading(true);
+        const data = await uploadToS3(file);
+        console.log("meow", data);
+        if (!data?.file_key || !data.file_name) {
+          toast.error("Something went wrong");
+          return;
         }
-    })
-    const {getRootProps, getInputProps} = useDropzone({
-        accept: {"application/pdf": [".pdf"]},
-        maxFiles: 1,
-        onDrop: async (acceptedFiles) => {
-            // console.log(acceptedFiles);
-            const file = acceptedFiles[0];
-            if(file.size > 10*1024*1024){
-                // bigger than 10mb
-                // alert("File size is too big");
-                toast.error("File size is too big");
-                return;
-            }
+        mutate(data, {
+          onSuccess: ({ chat_id }) => {
+            toast.success("Chat created!");
+            router.push(`/chat/${chat_id}`);
+          },
+          onError: (err) => {
+            toast.error("Error creating chat");
+            console.error(err);
+          },
+        });
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setUploading(false);
+      }
+    },
+  });
+  return (
+    <div className="p-2 bg-white rounded-xl">
+      <div
+        {...getRootProps({
+          className:
+            "border-dashed border-2 rounded-xl cursor-pointer bg-gray-50 py-8 flex justify-center items-center flex-col",
+        })}
+      >
+        <input {...getInputProps()} />
+        {uploading || isLoading ? (
+          <>
+            {/* loading state */}
+            <Loader2 className="h-10 w-10 text-blue-500 animate-spin" />
+            <p className="mt-2 text-sm text-slate-400">
+              Spilling Tea to GPT...
+            </p>
+          </>
+        ) : (
+          <>
+            <Inbox className="w-10 h-10 text-blue-500" />
+            <p className="mt-2 text-sm text-slate-400">Drop PDF Here</p>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
 
-            try {
-                setUploading(true);
-                const data = await uploadToS3(file);
-                // console.log(data);
-                if(!data?.file_key|| !data?.file_name){
-                    // alert("Error uploading file");
-                    toast.error("Error uploading file");
-                    return;
-                }
-
-                mutate(data, {
-                    onSuccess:({chat_id}) => {
-                        toast.success(`Chat created with id ${chat_id}`);
-                        // window.location.href = `/chat/${chat_id}`;
-                        // router.push(`/chat/${chat_id}`);
-
-                    },
-                    onError: (error) => {
-                        toast.error("Error creating chat");
-                        console.log(error);
-                    }
-                });
-                
-            } catch (error) {
-                console.log(error);
-            }finally{
-                setUploading(false);
-            }
-
-        }
-    });
-    return ( 
-        <div className="p-2 bg-white rounded-xl">
-            <div {...getRootProps({
-                className: 'dropzone border-dashed border-2 rounded-xl cursor-pointer bg-gray-50 py-8 flex justify-center items-center flex-col',
-            })}>
-                <input type="text" className="" {...getInputProps()} />
-                {uploading || isLoading ? (
-                    <>
-                        <Loader2 className="w-10 h-10 text-blue-500 animate-spin" />
-                        <p className="mt-2 text-sm text-slate-400">
-                            Uploading...
-                        </p>
-                    </>
-                ) :(
-
-                    <>
-                        <Inbox className="w-10 h-10 text-blue-500" />
-                        <p className="mt-2 text-sm text-slate-400">
-                            Drag n drop some files here, or click to select files
-                        </p>
-                    </>
-                )}
-            </div>
-        </div>
-     );
-}
- 
 export default FileUpload;
